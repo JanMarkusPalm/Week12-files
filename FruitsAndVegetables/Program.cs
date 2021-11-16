c25ac5fcb76e335b6e505c8889f06a368701e380
Opilane TTHK <opilane@tthk.ee>
2021-11-16 16:22:39 +0200

    sheep

---
+++ FruitsAndVegetables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace FruitsAndVegetables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruitPath = @"C:\Users\opilane\samples\Fruit.txt";
+            string[] veggieData = File.ReadAllLines(fruitPath);
+            string veggiePath = @"C:\Users\opilane\samples\Veggie.txt"; ;
+            string[] FruitData = File.ReadAllLines(veggiePath);
+
+            File.WriteAllLines(fruitPath, FruitData);
+            File.WriteAllLines(veggiePath, veggieData);
+
+        }
+    }
+}
